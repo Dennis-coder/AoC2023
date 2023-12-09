aa048a9a4ffeea78070fa7e01473b4da79d5dbea
Dennis Christensen <dennis.f.christensen@gmail.com>
2023-12-09 06:41:21 +0100

    day9

---
+++ day9/solution.py
@@ -0,0 +1,46 @@
+from functools import cache, cmp_to_key
+from queue import Queue, PriorityQueue
+from heapq import heappush, heappop
+import re
+from copy import deepcopy
+from math import *
+
+
+def parse(file_name):
+    def derivatives(history):
+        cur = history
+        derivatives = [history]
+        while True:
+            derivative = []
+            for i in range(1, len(cur)):
+                derivative.append(cur[i] - cur[i-1])
+            if all([x == 0 for x in derivative]):
+                return derivatives
+            derivatives.append(derivative)
+            cur = derivative
+
+    with open(file_name, "r") as file:
+        data = [
+            derivatives([
+                int(x)
+                for x in line.split()
+            ])
+            for line in file.read().splitlines()
+        ]
+    return data
+
+def part1(data):
+    sum = 0
+    for derivatives in data:
+        for derivative in derivatives:
+            sum += derivative[-1]
+    return sum 
+
+def part2(data):
+    sum = 0
+    for derivatives in data:
+        val = 0
+        for derivative in derivatives[::-1]:
+            val = derivative[0] - val
+        sum += val
+    return sum 
